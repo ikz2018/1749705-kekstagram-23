3435d2cf36ac89287fab57ecf4fec37436655b55
ikz2018 <ikz2018@bk.ru>
2021-06-05 22:39:55 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
-// eslint-disable-next-line no-unused-vars
-function getRandomRang(myMin, myMax) {
-  if (myMin <= myMax) {
-    return Math.floor( Math.random() * (myMax - myMin + 1) + myMin );
-  } else if (myMin === myMax) {
-    return myMin;
+const isPositiveNumber = (value) => typeof value === 'number' && value >=0;
+
+function getRandomBetween(min = 0, max = 0) {
+  if (!isPositiveNumber(min) || !isPositiveNumber(max)) {
+    throw new Error('Аргументы должны быть числами.');
   }
-  throw new Error('Вы задали неверный диапазон');
+  const from = Math.min(min, max);
+  const to = Math.max(min, max);
+  return Math.floor(Math.random() * (to - from + 1)) + from;
+}
+try {
+  getRandomBetween();
+} catch (err) {
+  // eslint-disable-next-line no-console
+  console.log(err); //не выходит в консоль
 }
 
 // eslint-disable-next-line no-unused-vars
