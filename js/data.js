16e24e2f8b803e9178a30bb4e9e1bfcdb99ab431
ikz2018 <78225068+ikz2018@users.noreply.github.com>
2021-06-23 01:51:44 +0600

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,17 @@
+const FIRST_COMMENT = 1;
+const SECOND_COMMENT = 2;
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
+const NUMBER_OF_COMMENTS = 1000;
+const FIRST_AVATAR = 1;
+const SIXTH_AVATAR = 6;
+const DESCRIPTION = ['Природа', 'Искусство', 'Техника'];
+const MESSAGE = ['Всё отлично!',
+'В целом всё неплохо. Но не всё.',
+'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+
+export {FIRST_COMMENT, SECOND_COMMENT, MIN_LIKES, MAX_LIKES, NUMBER_OF_COMMENTS,
+        FIRST_AVATAR, SIXTH_AVATAR, DESCRIPTION, MESSAGE};
