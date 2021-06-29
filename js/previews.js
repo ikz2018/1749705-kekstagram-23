b2cad98784a666b9bae0c01a99b6ae1f09d7ccd9
ikz2018 <78225068+ikz2018@users.noreply.github.com>
2021-06-29 22:24:12 +0600

    Update previews.js

--- js/previews.js
+++ js/previews.js
@@ -1,15 +1,23 @@
+import {showBigPicture} from './big-photo.js'
+
 const PHOTOS_TEMPLATE = document.querySelector('#picture').content;
 const PHOTOS_CONTAINER = document.querySelector('.pictures');
+
+const onPhotoClick = (photo) => () => {
+  showBigPicture(photo)
+};
 
 const fillPhotos = (photos) => {
   const fragment = document.createDocumentFragment();
 
-  photos.forEach(({url, likes, comments}) => {
+  photos.forEach((photo) => {
+    const {url, likes, comments} = photo;
     const pictureElement = PHOTOS_TEMPLATE.cloneNode(true);
 
     pictureElement.querySelector('.picture__img').src = url;
     pictureElement.querySelector('.picture__likes').textContent = likes;
     pictureElement.querySelector('.picture__comments').textContent = comments.length;
+    pictureElement.querySelector('.picture').addEventListener('click', onPhotoClick(photo))
     fragment.appendChild(pictureElement);
   });
 
