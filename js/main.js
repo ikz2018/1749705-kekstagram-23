8e22cc64d352b5e452bd5027a3c4573987b0dd59
ikz2018 <ikz2018@bk.ru>
2021-07-06 16:38:32 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import {getRandomPhotos} from './data.js';
 import {fillPhotos} from './previews.js';
-import './upload-form.js';
+// eslint-disable-next-line no-unused-vars
+import {openUploadForm,closeUploadForm,commentValidity,hashtagValidity} from './upload-form.js';
 
 const photos = getRandomPhotos(25);
 
