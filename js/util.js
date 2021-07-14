123c289536c9de9b2feff97c428d1df6cf8a0fc7
ikz2018 <78225068+ikz2018@users.noreply.github.com>
2021-07-15 00:01:02 +0600

    Update util.js

--- js/util.js
+++ js/util.js
@@ -55,10 +55,37 @@ const fillBy = (count, cb) => {
   return result;
 };
 
+const isEscEvent = (evt) => evt.key === 'Escape' || evt.key === 'Esc';
+
+const ALERT_SHOW_TIME = 5000;
+
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = 100;
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = 0;
+  alertContainer.style.top = 0;
+  alertContainer.style.right = 0;
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+};
+
 export {
   getRandomItem,
   fillBy,
   createGetId,
   createGetRandomItem,
-  getRandomFloat
+  getRandomFloat,
+  showAlert,
+  isEscEvent
 };
