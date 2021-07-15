40ddf49901658619899ee1cbf5c56b391951672a
ikz2018 <78225068+ikz2018@users.noreply.github.com>
2021-07-15 16:48:45 +0600

    Update util.js

--- js/util.js
+++ js/util.js
@@ -57,26 +57,20 @@ const fillBy = (count, cb) => {
 
 const isEscEvent = (evt) => evt.key === 'Escape' || evt.key === 'Esc';
 
+const ALERT_TEMPLATE = document.querySelector('#alert').content;
+const ALERT_MESSAGE = 'Не удалось получить данные. Попробуйте ещё раз';
 const ALERT_SHOW_TIME = 5000;
 
-const showAlert = (message) => {
-  const alertContainer = document.createElement('div');
-  alertContainer.style.zIndex = 100;
-  alertContainer.style.position = 'absolute';
-  alertContainer.style.left = 0;
-  alertContainer.style.top = 0;
-  alertContainer.style.right = 0;
-  alertContainer.style.padding = '10px 3px';
-  alertContainer.style.fontSize = '30px';
-  alertContainer.style.textAlign = 'center';
-  alertContainer.style.backgroundColor = 'red';
+const showAlert = () => {
+  const allertElement = ALERT_TEMPLATE.cloneNode(true);
+  const allertInner = allertElement.querySelector('.alert__inner');
 
-  alertContainer.textContent = message;
+  allertInner.textContent = ALERT_MESSAGE;
 
-  document.body.append(alertContainer);
+  document.body.append(allertInner);
 
   setTimeout(() => {
-    alertContainer.remove();
+    allertElement.remove();
   }, ALERT_SHOW_TIME);
 };
 
